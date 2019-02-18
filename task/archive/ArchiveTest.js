5aabeffc5d941022becafd762598435c3694ba24
MasatoMakino <unagiinu.g@gmail.com>
2019-02-18 12:45:05 +0900

    アーカイブからの復元処理を追加。

--- task/archive/ArchiveTest.js
+++ task/archive/ArchiveTest.js
@@ -1,3 +1,6 @@
 const archiver = require("./IssueArchiver");
 
-archiver.archive();
+// archiver.archive();
+
+// archiver.rollback("./archive/atcoder/practice/practice_1/20190217_155528");
+archiver.rollback("./archive/atcoder/practice/practice_1/");
--- task/archive/IssueArchiver.js
+++ task/archive/IssueArchiver.js
@@ -6,6 +6,7 @@ const path = require("path");
 const makeDir = require("make-dir");
 const datefns = require("date-fns");
 const cpx = require("cpx");
+const rimraf = require("rimraf");
 
 module.exports = {
   /**
@@ -31,5 +32,91 @@ module.exports = {
     cpx.copySync("./src/**/!(_|template)*.js", archiveDir + "/src/");
     cpx.copySync("./dist/**/*.js", archiveDir + "/dist/");
     cpx.copySync(testConfig.testDir + "**/*", archiveDir + "/test/");
+  },
+  /**
+   * 指定されたアーカイブディレクトリからファイルを復元する。
+   * @param {string} dirPath
+   *   ディレクトリパス。
+   *   タイムスタンプ付きのディレクトリを指定すると、そのディレクトリのファイルを復元する。
+   *   issueフォルダを指定すると、そのディレクトリ内の最新のアーカイブを復元する。
+   * @param {*} isDryRun
+   */
+  rollback(dirPath, isDryRun) {
+    if (isDryRun === undefined) isDryRun = false;
+
+    if (dirPath == null || dirPath == "") {
+      console.log("復元対象を指定してください。");
+      return;
+    }
+
+    const archiveDir = this.getArchiveDir(dirPath);
+    if (archiveDir == null) {
+      return;
+    }
+    console.log("restore from " + archiveDir);
+    if (isDryRun) {
+      return;
+    }
+
+    fs.copyFileSync(
+      path.resolve(archiveDir, issueInfo.JSON_NAME),
+      issueInfo.JSON_PATH()
+    );
+    fs.copyFileSync(
+      path.resolve(archiveDir, "src", "index.js"),
+      "./src/index.js"
+    );
+
+    //既存のテストケース削除
+    rimraf.sync(testConfig.testDir);
+    rimraf.sync("./dist");
+    cpx.copySync(path.resolve(archiveDir, "dist") + "/**/*", "./dist/");
+    cpx.copySync(
+      path.resolve(archiveDir, "test") + "/**/*",
+      testConfig.testDir
+    );
+  },
+  /**
+   * 指定されたディレクトリにissue_infoが存在するか確認する
+   * @param {string}} dirPath
+   */
+  getArchiveDir(dirPath) {
+    const resolvedPath = path.resolve(dirPath);
+
+    console.log(resolvedPath);
+    if (!fs.existsSync(resolvedPath)) {
+      console.log("指定されたディレクトリが存在しません。".red);
+      return;
+    }
+
+    if (this.hasIssueJson(dirPath)) {
+      return resolvedPath;
+    }
+
+    const list = fs
+      .readdirSync(resolvedPath)
+      .sort()
+      .reverse()
+      .filter(val => {
+        return /^[0-9_]*$/.test(val);
+      });
+    const listHead = path.resolve(dirPath, list[0]);
+    if (this.hasIssueJson(listHead)) {
+      return listHead;
+    }
+
+    console.log(
+      "ディレクトリ内にアーカイブフィイルを発見できませんでした。".red
+    );
+    return;
+  },
+
+  /**
+   * 指定されたディレクトリに課題情報が存在するか確認する。
+   * @param {string} dir
+   */
+  hasIssueJson(dir) {
+    const archivedJsonPath = path.resolve(dir, issueInfo.JSON_NAME);
+    return fs.existsSync(archivedJsonPath);
   }
 };
